aaf4c289e9b31ffd644db36f11f11945fb39fe4f
Brahim Ben Younes <37020430+BrahimBenyounes@users.noreply.github.com>
2024-06-04 11:02:48 +0100

    Update Stock.service.ts

--- angular-front/src/app/shared/Service/Stock.service.ts
+++ angular-front/src/app/shared/Service/Stock.service.ts
@@ -7,7 +7,7 @@ import { HttpClient} from '@angular/common/http';
   providedIn: 'root'
 })
 export class StockService {
-  readonly API_URL = 'http://localhost:8084/stock';
+  readonly API_URL = 'http://192.168.186.186:8084/stock';
 
   constructor(private httpClient: HttpClient) { }
 
